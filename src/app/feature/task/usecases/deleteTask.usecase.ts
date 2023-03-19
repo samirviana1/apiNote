d5ec0be850a8eaa41b3b759efd702cc4b1c18372
SamirBatista <samir_viana1@hotmail.com>
2023-03-18 23:01:38 -0300

    EDIT

--- src/app/feature/task/usecases/deleteTask.usecase.ts
+++ src/app/feature/task/usecases/deleteTask.usecase.ts
@@ -2,19 +2,3 @@ import {TaskRepository} from "../repositories/task.repository";
 import {TaskDTO} from "../dtos/createTask.dto";
 import {Task} from "../../../models";
 import {CustomError} from "../../../shared/utils/errors/custom.error";
-
-// export class DeleteTaskUserUsecase {
-//   #repository: TaskRepository;
-
-//   constructor(repository: TaskRepository) {
-//     this.#repository = repository;
-//   }
-
-//   // public async execute(dto: TaskDTO): Promise<Task> {
-//   //   const result = this.#repository.delTask(dto.uid!);
-//   //   if (!result) {
-//   //     throw new CustomError("erro", 404);
-//   //   }
-//   //   return result;
-//   // }
-// }
--- src/app/feature/task/usecases/getUserTask.usecase.ts
+++ src/app/feature/task/usecases/getUserTask.usecase.ts
@@ -2,19 +2,3 @@ import {TaskRepository} from "../repositories/task.repository";
 import {TaskDTO} from "../dtos/createTask.dto";
 import {Task} from "../../../models";
 import {CustomError} from "../../../shared/utils/errors/custom.error";
-
-// export class GetUserTaskUsecase {
-//   #repository: TaskRepository;
-
-//   constructor(repository: TaskRepository) {
-//     this.#repository = repository;
-//   }
-
-// public async execute(dto: TaskDTO): Promise<Task> {
-//   const taskIdUser = this.#repository.getTask(dto.userUid);
-//   if (!taskIdUser) {
-//     throw new CustomError("erro", 404);
-//   }
-//   return taskIdUser;
-// }
-//}
