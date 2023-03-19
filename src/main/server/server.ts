b61b8dcddca5c4d0243ed6863f259d51bf5fc0fc
SamirBatista <samir_viana1@hotmail.com>
2023-03-19 17:47:05 -0300

    editGetServerun

--- src/main/server/server.ts
+++ src/main/server/server.ts
@@ -1,10 +1,13 @@
-import { createServer } from '../config/server.config';
-import 'dotenv/config';
+import {Response} from "express";
+import {createServer} from "../config/server.config";
+import "dotenv/config";
 
 export const runServer = () => {
-    const app = createServer();
-
-    app.listen(process.env.PORT, () => {
-        console.log(`🤘 - Servidor rodando na porta ${process.env.PORT}`);
-    });
+  const app = createServer();
+  app.get("/", (_, res: Response) => {
+    res.send("<h1>Api Vagas</h1>");
+  });
+  app.listen(process.env.PORT, () => {
+    console.log(`🤘 - Servidor rodando na porta ${process.env.PORT}`);
+  });
 };
