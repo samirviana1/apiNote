330da43aaaf59c69939e89f9dfb1eee4a84e62a1
SamirBatista <samir_viana1@hotmail.com>
2023-03-20 19:42:14 -0300

    routesTask

--- src/app/feature/task/routes/task.router.ts
+++ src/app/feature/task/routes/task.router.ts
@@ -1,9 +1,24 @@
 import {Router, Request, Response} from "express";
+import {taskValidator} from "../middlewares/task.validator";
+import {authMiddleware} from "../../../shared/middlewares/auth.middleware";
+import {TaskController} from "../controllers/task.controlller";
+import {taskDadosUpdate} from "../middlewares/taskDadosUpdate";
 
 export const taskRoutes = () => {
   const route = Router();
 
   route.post("/", (req: Request, res: Response) => {});
-
+  route.post(
+    "/taskCreate",
+    [taskValidator, authMiddleware],
+    TaskController.createTask
+  );
+  route.get("/getUsers", [authMiddleware], TaskController.getAllUserTask);
+  route.post(
+    "updateTask",
+    [authMiddleware, taskDadosUpdate],
+    TaskController.updateTask
+  );
+  route.delete("deleteTask", [authMiddleware], TaskController.deleteTask);
   return route;
 };
