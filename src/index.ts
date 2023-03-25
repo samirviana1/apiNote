72a781a5ff588c1c7aea5ed9760c2304740f7d2c
SamirBatista <samir_viana1@hotmail.com>
2023-03-25 12:19:54 -0300

    tsconfigEnvIndex

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import {RedisConnection} from "./main/database/redis.connection";
 import {DatabaseConnection} from "./main/database/typeorm.connection";
 import {runServer} from "./main/server/server";
 
-Promise.all([DatabaseConnection.connect()])
+Promise.all([DatabaseConnection.connect(), RedisConnection.connect()])
   .then(runServer)
   .catch((error: any) => {
     console.log("Erro ao iniciar");
