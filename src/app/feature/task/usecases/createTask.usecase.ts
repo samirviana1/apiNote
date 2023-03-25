19257ef0f41bb4fff1a28bba67f4116291bd4642
SamirBatista <samir_viana1@hotmail.com>
2023-03-25 19:14:56 -0300

    cache

--- src/app/feature/task/usecases/createTask.usecase.ts
+++ src/app/feature/task/usecases/createTask.usecase.ts
@@ -2,6 +2,7 @@ import {v4} from "uuid";
 import {Task} from "../../../models";
 import {TaskDTO} from "../dtos/createTask.dto";
 import {TaskRepository} from "../repositories/task.repository";
+import {CacheRepository} from "src/app/shared/database/repository/cache.repository";
 
 export class CreateTaskUseCase {
   #repository: TaskRepository;
@@ -11,9 +12,18 @@ export class CreateTaskUseCase {
   }
 
   public async execute(dto: TaskDTO): Promise<Task> {
-    dto.uid = v4();
+    //repository cache
+    const cacheRepository = new CacheRepository();
 
+    dto.uid = v4();
     const task = await this.#repository.create(dto);
+
+    //getcacheMetod
+    const noteCacheList = await cacheRepository.get("TASK_LIST");
+    //metod push da task na list do cache
+    noteCacheList.push(task);
+    //set metodcache para setar task_list no cache
+    await cacheRepository.set("TASK_LIST", noteCacheList);
 
     return task;
   }
--- src/app/feature/task/usecases/getUserTask.usecase.ts
+++ src/app/feature/task/usecases/getUserTask.usecase.ts
@@ -2,6 +2,7 @@ import {TaskRepository} from "../repositories/task.repository";
 import {TaskDTO} from "../dtos/createTask.dto";
 import {Task} from "../../../models";
 import {CustomError} from "../../../shared/utils/errors/custom.error";
+import {CacheRepository} from "src/app/shared/database/repository/cache.repository";
 
 export class GetAllTaskUserUsecase {
   #repository: TaskRepository;
@@ -10,10 +11,14 @@ export class GetAllTaskUserUsecase {
   }
 
   public async execute(userUid: any): Promise<Task[] | null> {
+    const cacheRepository = new CacheRepository();
+    cacheRepository.del("LIST_TASK_USER");
     const tasksUser = this.#repository.getAllTaskUser(userUid);
     if (!tasksUser) {
       throw new CustomError("Não há recados no seu sistema!", 404);
     }
+    const taskList = (await cacheRepository.get("LIST_TASK_USER")) ?? [];
+    await cacheRepository.set("LIST_TASK_USER", taskList);
     return tasksUser;
   }
 }
