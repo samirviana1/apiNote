c3da067b6170e62e6f76f3129cb3b964816c86b1
SamirBatista <samir_viana1@hotmail.com>
2023-03-29 20:37:19 -0300

    editsDataTestPackCache

--- src/app/feature/task/usecases/deleteTask.usecase.ts
+++ src/app/feature/task/usecases/deleteTask.usecase.ts
@@ -2,19 +2,22 @@ import {TaskRepository} from "../repositories/task.repository";
 import {TaskDTO} from "../dtos/createTask.dto";
 import {Task} from "../../../models";
 import {CustomError} from "../../../shared/utils/errors/custom.error";
+import {CacheRepository} from "src/app/shared/database/repository/cache.repository";
 
 export class DeleteTaskUserUsecase {
   #repository: TaskRepository;
+  #cacheRepository: CacheRepository;
 
   constructor(repository: TaskRepository) {
     this.#repository = repository;
+    this.#cacheRepository = new CacheRepository();
   }
 
-  public async execute(uid: any): Promise<void> {
-    const deleteTaskUser = this.#repository.delTask(uid);
-    if (!deleteTaskUser) {
-      throw new CustomError("O sistema não encontrou a mensagem!", 404);
-    }
+  public async execute(id: any): Promise<void> {
+    const taskList = (await this.#cacheRepository.get("TASK_LIST")) ?? [];
+    const deleteTask = taskList.filter((task: any) => task.id === id);
+    await this.#cacheRepository.set("TASK_LIST", deleteTask);
+    const deleteTaskUser = await this.#repository.delTask(id);
     return deleteTaskUser;
   }
 }
--- src/main/config/database.config.ts
+++ src/main/config/database.config.ts
@@ -1,10 +1,17 @@
 import {DataSource} from "typeorm";
 import "dotenv/config";
+import {CreateTableUsers1677785802931} from "src/app/shared/database/migrations/1677785802931-CreateTableUsers";
+import {CreateTableTasks1677785941823} from "src/app/shared/database/migrations/1677785941823-CreateTableTasks";
+import {
+  DefaultEntity,
+  TaskEntity,
+  UserEntity,
+} from "src/app/shared/database/entities";
 
 export default new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: false,
-  entities: ["src/app/shared/database/entities/**/*.ts"],
-  migrations: ["src/app/shared/database/migrations/**/*.ts"],
+  entities: [DefaultEntity, TaskEntity, UserEntity],
+  migrations: [CreateTableUsers1677785802931, CreateTableTasks1677785941823],
 });
