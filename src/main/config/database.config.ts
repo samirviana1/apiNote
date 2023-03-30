b57095bbad4215464b487618d13279ef2000ec77
SamirBatista <samir_viana1@hotmail.com>
2023-03-30 16:46:46 -0300

    erro-sanado

--- src/main/config/database.config.ts
+++ src/main/config/database.config.ts
@@ -1,13 +1,13 @@
 import {DataSource} from "typeorm";
 import "dotenv/config";
-import {appEnv} from "src/app/env/app.env";
-import {CreateTableUsers1677785802931} from "src/app/shared/database/migrations/1677785802931-CreateTableUsers";
-import {CreateTableTasks1677785941823} from "src/app/shared/database/migrations/1677785941823-CreateTableTasks";
+import {appEnv} from "../../app/env/app.env";
+import {CreateTableUsers1677785802931} from "../../app/shared/database/migrations/1677785802931-CreateTableUsers";
+import {CreateTableTasks1677785941823} from "../../app/shared/database/migrations/1677785941823-CreateTableTasks";
 import {
   DefaultEntity,
   TaskEntity,
   UserEntity,
-} from "src/app/shared/database/entities";
+} from "../../app/shared/database/entities";
 
 export default new DataSource({
   type: "postgres",
